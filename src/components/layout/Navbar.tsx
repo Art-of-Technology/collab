cd58f71ab0e6b4c707dfb0ae69d9f6c4ef4a8764
redoh <ferit@weezboo.com>
2025-03-27 19:07:26 +0000

    Remove logoIcon

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -23,7 +23,6 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import LogoIcon from "@/components/icons/LogoIcon";
 import { CustomAvatar } from "@/components/ui/custom-avatar";
 import { useUiContext } from "@/context/UiContext";
 
