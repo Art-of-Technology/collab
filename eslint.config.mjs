8e8ddb9ec4d4eddbd63ae07b9fb8c1dc9316bf77
S. Ferit Arslan <ferit@weezboo.com>
2025-06-30 19:26:48 +0100

    Update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,12 +8,16 @@ const __dirname = dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
-
+ //disable . Use 'const' instead.  prefer-const
+ //disable Error: `"` can be escaped with `&quot;`, `&ldquo;`, `&#34;`, `&rdquo;`.  react/no-unescaped-entities 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "off"
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "prefer-const": "off",
+      "react/no-unescaped-entities": "off"
     }
   }
 ];
